206a20837f47cd900d7aeb992bee053ef16a135d
Isaac Morales Durán <isaac@iaa.es>
2016-07-30 06:41:13 +0200

    added autoclear.sh

---
+++ autoclear.sh
@@ -0,0 +1,25 @@
+rm -r autom4te.cache;
+rm -r m4;
+rm aclocal.m4
+rm compile;
+rm config.guess;
+rm config.h;
+rm config.h.in;
+rm config.log;
+rm config.status;
+rm config.sub;
+rm configure;
+rm depcomp;
+rm fmpt_saa.log;
+rm install-sh;
+rm libtool;
+rm ltmain.sh;
+rm Makefile;
+rm Makefile.in;
+rm data/Makefile;
+rm data/Makefile.in;
+rm src/Makefile;
+rm src/Makefile.in;
+rm missing;
+rm stamp-h1;
+rm test-driver;
